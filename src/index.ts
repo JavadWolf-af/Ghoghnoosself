b3cbea2d295bcb5486adc260529057752be479fc
JavadWolf <Ghoghnoossefid@gmail.com>
2026-06-26 17:49:55 +0330

    feat: migrate to PostgreSQL + Drizzle ORM (v2.0)

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,18 @@
-import "./bot";
+import { initDb } from "./db/init";
 import { logger } from "./lib/logger";
 
-logger.info("Ghoghnoosself bot is running...");
+logger.info("Initializing database...");
+initDb()
+  .then(() => {
+    logger.info("Database ready. Starting bot...");
+    return import("./bot");
+  })
+  .catch((err) => {
+    logger.error({ err }, "Failed to initialize database");
+    process.exit(1);
+  });
 
-process.on("uncaughtException", (err) => logger.error({ err }, "uncaughtException"));
+process.on("uncaughtException",  (err)    => logger.error({ err }, "uncaughtException"));
 process.on("unhandledRejection", (reason) => logger.error({ reason }, "unhandledRejection"));
-process.on("SIGINT", () => { logger.info("Shutting down..."); process.exit(0); });
+process.on("SIGINT",  () => { logger.info("Shutting down..."); process.exit(0); });
 process.on("SIGTERM", () => { logger.info("Shutting down..."); process.exit(0); });
