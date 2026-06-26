024107adf8f3448048c1d35a58fc83ae20c0044f
JavadWolf <Ghoghnoossefid@gmail.com>
2026-06-26 16:26:44 +0330

    chore: remove src/index.ts

--- src/index.ts
+++
@@ -1,9 +0,0 @@
-import "./bot";
-import { logger } from "./lib/logger";
-
-logger.info("Ghoghnoosself bot is running...");
-
-process.on("uncaughtException", (err) => logger.error({ err }, "uncaughtException"));
-process.on("unhandledRejection", (reason) => logger.error({ reason }, "unhandledRejection"));
-process.on("SIGINT", () => { logger.info("Shutting down..."); process.exit(0); });
-process.on("SIGTERM", () => { logger.info("Shutting down..."); process.exit(0); });
